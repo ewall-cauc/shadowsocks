3930cdcb61225c9c9fc700f6c356a1c3fd832634
yaodi_cauc@163.com <yaodi_cauc@163.com>
2016-06-01 04:05:51 +0800

    delete mail address

--- shadowsocks/__init__.py
+++ shadowsocks/__init__.py
@@ -13,7 +13,5 @@
 # WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
 # License for the specific language governing permissions and limitations
 # under the License.
-
-#add my-mail-address yaodi_cauc@163.com
 from __future__ import absolute_import, division, print_function, \
     with_statement
